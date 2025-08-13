56b2f52177b52e2a057be5dee95b375edace5c0f
imsturdyo <olusamo50@gmail.com>
2025-08-13 02:27:35 +0100

    init commit

---
+++ src/main/java/com/takehome/carselection/CarselectionApplication.java
@@ -0,0 +1,13 @@
+package com.takehome.carselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarselectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarselectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takehome/carselection/CarselectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takehome.carselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarselectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
